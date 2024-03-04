a163a681d2ce467fb73aaef6434581ad0f21e449
SilverRon <gregorypaek94@gmail.com>
2024-03-05 00:41:32 +0900

    Add github base url on conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ release = '0.9'
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+html_baseurl = 'https://silverron.github.io/gppy/'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
